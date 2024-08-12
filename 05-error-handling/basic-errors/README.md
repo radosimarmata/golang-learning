7e7c05334e66ef41814fd23e278cec13ec3656e5
radosimarmata27 <simarmatarado@gmail.com>
2024-08-12 16:48:10 +0700

    add basic errors

---
+++ 05-error-handling/basic-errors/README.md
@@ -0,0 +1,8 @@
+# Basic Error Handling
+
+Contoh kode ini menunjukkan cara mengembalikan dan memeriksa error dalam Go.
+
+## Cara Menjalankan
+
+1. Navigasikan ke folder `basic-errors`.
+2. Jalankan perintah `go run main.go`.
---
+++ 05-error-handling/basic-errors/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func divide(a, b float64) (float64, error) {
+	if b == 0 {
+		return 0, errors.New("cannow divide by zero")
+	}
+	return a / b, nil
+}
+
+func main() {
+	result, err := divide(10, 0)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Result:", result)
+}
